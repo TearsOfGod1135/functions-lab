720ddf2ff5e4f0da0fb34b0a199ff1b24fd78793
TearsOfGod <leviskariuki2@gmail.com>
2026-04-21 09:28:18 +0300

    Completed JavaScript functions lab

--- index.js
+++ index.js
@@ -1,7 +1,32 @@
 
+// Function 1: calculateTax
+function calculateTax(amount) {
+  return amount * 0.10;
+}
 
+// Function 2: convertToUpperCase
+function convertToUpperCase(text) {
+  return text.toUpperCase();
+}
 
+// Function 3: findMaximum
+function findMaximum(num1, num2) {
+  return num1 > num2 ? num1 : num2;
+}
 
+// Function 4: isPalindrome
+function isPalindrome(word) {
+  const normalized = word.toLowerCase();
+  const reversed = normalized.split('').reverse().join('');
+  return normalized === reversed;
+}
+
+// Function 5: calculateDiscountedPrice
+function calculateDiscountedPrice(originalPrice, discountPercentage) {
+  const discount = (discountPercentage / 100) * originalPrice;
+  return originalPrice - discount;
+}
 
 // This is required for the test to function properly  
 module.exports = { calculateTax, convertToUpperCase, findMaximum, isPalindrome, calculateDiscountedPrice };
+
